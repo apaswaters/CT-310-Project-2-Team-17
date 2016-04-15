1b921ba071297ee127b84d7249ccf64a426e1bca
Austin Paswaters <apaswate@sacramento.cs.colostate.edu>
2016-04-15 14:41:46 -0600

    hello github

--- Pets_List.php
+++ Pets_List.php
@@ -13,6 +13,27 @@
 	</div>
 
 	<div id = "body">
+
+		<form role="form" method="post">
+ 			<input type="text" class="form-control" id="keyword" placeholder="Search Here">
+ 		</form>
+ 		<ul id="content"></ul>
+    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    	<script type="text/javascript">
+ 		$(document).ready(function() {
+ 			$('#keyword').on('input', function() {
+ 				var searchKey = $(this).val();
+ 				if (searchKey.length >= 3) {
+ 					$.post('search.php', { keywords: searchKey }, function(data) {
+ 						$('ul#content').empty()
+ 						$.each(data, function() {
+ 							$('ul#content').append('<li><a href="Pets_List.php?id=' + this.id + '">' + this.title + '</a></li>');
+ 						});
+ 					}, "json");
+ 				}
+ 			});
+ 		});
+ 		</script>
   
 			<h1>Pinky</h1>
 			<a href="Pinky.php"><img src="Pinky_gallery.jpg" alt="Smiley face" width="300" height="300" align="middle"> 
